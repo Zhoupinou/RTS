36b6a5f21d4eaf013eb2de9a91be4b59e25528cd
Roquekinsley <116672482+Roquekinsley@users.noreply.github.com>
2023-07-23 22:26:20 +0200

    Delete Assets/Script directory

--- Assets/Script/UltimateTower.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UltimateTower : MonoBehaviour
-{
-    // Ceci est juste un marqueur pour indiquer que ce GameObject est une Ultimate Tower.
-    // Vous pouvez ajouter d'autres variables ou méthodes ici si nécessaire.
-}
